d48608ff1fc8c6f19367f645344671e109f85288
Charlottepeddle12 <charlotte.peddle12@ed.cna.nl.ca>
2025-05-27 14:49:30 -0230

    Added some bootstrap styling

--- Assign1/src/Movie.jsx
+++ Assign1/src/Movie.jsx
@@ -1,9 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 function Movie({name, year, rating}){
     return(
         <>
+        <div class="m-3">
             <h3>Movie: {name}</h3>
             <p>Year released: {year}</p>
             <p>Rating: {rating}</p>
+        </div>
         </>
     )
 }
--- Assign1/src/NavBar.jsx
+++ Assign1/src/NavBar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 function NavBar(){
     let [buttontext, setText] = useState("Login");
@@ -6,7 +8,7 @@ function NavBar(){
         <>
         <h1>Assignment 1</h1>
 
-        <button onClick={()=>{
+        <button type="button" class="btn btn-primary" onClick={()=>{
             if(buttontext == "Login")
             {
                 setText("Logout");
